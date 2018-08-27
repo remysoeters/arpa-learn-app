1aeef1fc449870ff940ce0173cb72b5ea84d0502
Remy Soeters <RSoeters@arpa.local>
2018-08-27 13:46:24 +0200

    server.js port added

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.set('view engine', 'hbs')
 hbs.registerPartials(__dirname + '/views/partials')
 
 // server config
-let port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
