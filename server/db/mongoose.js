eb8c77ad3534d719ad48874f2dc63bea64d133a3
Remy Soeters <RSoeters@arpa.local>
2018-08-28 12:25:16 +0200

    mongoose veranderd

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,8 @@ const mongoose = require ('mongoose')
 
 const databaseName = 'arpa-learn-app'
 mongoose.Promise = global.Promise
-const db = "mongodb://remy.soeters:B14rd3r822!@ds235732.mlab.com:35732/arpa-learn-database"
-mongoose.connect(db || `mongodb://localhost:27017/${databaseName}`)
+
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/${databaseName}`)
 
 module.exports = {
     mongoose
